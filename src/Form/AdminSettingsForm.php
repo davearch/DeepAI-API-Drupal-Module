214e8b42fd5734f037f7973c5392ee93ad0cfd7c
David Archuleta <darchuletajr@gmail.com>
2020-03-08 16:16:35 -0700

    I should just stick to one coding format

--- src/Form/AdminSettingsForm.php
+++ src/Form/AdminSettingsForm.php
@@ -8,12 +8,14 @@ namespace Drupal\deepai\Form;
 use Drupal\Core\Form\ConfigFormBase;  
 use Drupal\Core\Form\FormStateInterface;  
 
-class AdminSettingsForm extends ConfigFormBase {
+class AdminSettingsForm extends ConfigFormBase
+{
 
     /**  
      * {@inheritdoc}  
      **/
-    protected function getEditableConfigNames() {
+    protected function getEditableConfigNames()
+    {
         return [
             'deepai.adminsettings',
         ];
@@ -22,14 +24,16 @@ class AdminSettingsForm extends ConfigFormBase {
     /**
      * {@inheritdoc}
      */
-    public function getFormId() {
+    public function getFormId()
+    {
         return 'deepai_admin_settings';
     }
 
     /**
      * {@inheritdoc}
      */
-    public function buildForm(array $form, FormStateInterface $form_state) {
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
         $config = $this->config('deepai.adminsettings');
         $form['deepai_api_key'] = [
             '#type' => 'textfield',
@@ -51,14 +55,16 @@ class AdminSettingsForm extends ConfigFormBase {
     /**
      * {@inheritdoc}
      */
-    public function validateForm(array &$form, FormStateInterface $form_state) {
+    public function validateForm(array &$form, FormStateInterface $form_state)
+    {
         parent::validateForm($form, $form_state);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
         parent::submitForm($form, $form_state);
         $this->config('deepai.adminsettings')
           ->set('deepai_api_key', $form_state->getValue('deepai_api_key'))
